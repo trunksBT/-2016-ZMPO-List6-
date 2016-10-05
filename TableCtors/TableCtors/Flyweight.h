f21cdf1690134e07188d295c18ca9507b93cd249
korytkoBT <209826@student.pwr.wroc.pl>
2016-10-05 23:03:35 +0000

    Implement first use_case

--- TableCtors/TableCtors/Flyweight.h
+++ TableCtors/TableCtors/Flyweight.h
@@ -8,10 +8,12 @@ class Flyweight
 {
 public:
     static void createFlyweight(int inSize);
+    static void createFlyweight(std::vector<CTable*>& inCache);
     static void callCtor(std::string& command, std::string& receivedId);
     static void createCTable(std::vector<std::string>& command);
 public:
-    static void releaseResources();
+   static void releaseResources();
+   static void releaseResources(std::vector<CTable*>& inCache);
 private:
     static std::vector<CTable*> cache;
     static std::vector<std::string> command;
