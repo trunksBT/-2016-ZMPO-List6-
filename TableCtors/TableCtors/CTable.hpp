ef41b638b1289ca3b3f68fa865f3207b64d6f801
korytkoBT <209826@student.pwr.wroc.pl>
2016-10-06 23:54:24 +0000

    Add handling: memory, names, size, prints; Add UT-s

--- TableCtors/TableCtors/CTable.hpp
+++ TableCtors/TableCtors/CTable.hpp
@@ -7,18 +7,29 @@ class CTable
 {
 public:
    CTable();
+   CTable(int inSize);
    CTable(CTable& inVal);
    CTable(std::string inVal);
    ~CTable();
-public:
-   int getSize() const;
+   void copyCtor(CTable & inVal);
+   CTable& operator=(CTable& inObj);
 
 public:
-   void setName(std::string inVal);
+   int getSize() const;
+   int getVal(int idx) const;
+
+public:
+   void setName(std::string inName);
+   void setVal(int idx, int newVal);
+   void setSize(int inSize);
+
+private:
+   void allocateMemory(int inSize);
+   void deallocateMemory();
+   void copyMemory(CTable& inVal);
 
 private:
    std::string name_;
-   int* inTable_;
+   int* memory_;
    int size_;
 };
-
--- TableCtors/TableCtors/TableUT.cpp
+++ TableCtors/TableCtors/TableUT.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include "CTable.hpp"
 #include "Utils.hpp"
+#include <limits>
 
 using namespace defaultVals;
 namespace
@@ -16,7 +17,6 @@ protected:
    {}
 };
 
-//Test_MethodName_StateUnderTest_Expect_ExpectedBehavior
 TEST_F(CTableTests, defCTOR_getSize_DEFAULTSIZE_Expect_DEFAULTSIZE)
 {
    CTable* inVal = new CTable();
@@ -24,8 +24,76 @@ TEST_F(CTableTests, defCTOR_getSize_DEFAULTSIZE_Expect_DEFAULTSIZE)
 
    int rcVal = inVal->getSize();
 
-   EXPECT_EQ(expVal, rcVal);
+   ASSERT_EQ(expVal, rcVal);
    delete inVal;
+}
+
+TEST_F(CTableTests, defCTOR_setSize_0_Expect_0)
+{
+   int inSize = ZERO;
+   CTable* inVal = new CTable(inSize);
+   int expVal = ZERO;
+
+   int rcVal = inVal->getSize();
+
+   ASSERT_EQ(expVal, rcVal);
+   delete inVal;
+}
+
+TEST_F(CTableTests, copyCTOR_size5_Expect_size5)
+{
+   int inSize = FIVE;
+   int expVal = FIVE;
+   CTable* sourceVal = new CTable(inSize);
+
+   CTable* inVal = new CTable(*sourceVal);
+   int rcVal = inVal->getSize();
+
+   ASSERT_EQ(expVal, rcVal);
+   delete sourceVal;
+   delete inVal;
+}
+
+TEST_F(CTableTests, copyCTOR_withAssignCopyOperator_size5_Expect_size5)
+{
+   int inSize = FIVE;
+   int expVal = FIVE;
+   CTable* sourceVal = new CTable(inSize);
+
+   CTable* inVal = new CTable();
+   *inVal = *sourceVal;
+
+   int rcVal = inVal->getSize();
+
+   ASSERT_EQ(expVal, rcVal);
+   delete sourceVal;
+   delete inVal;
+}
+
+TEST_F(CTableTests, setVal_defaultCTORed_3_onIdx_0_Expect_3)
+{
+   int newVal = 500;
+   int idxForNewVal = 0;
+   CTable* sourceVal = new CTable();
+
+   int oldVal = sourceVal->getVal(idxForNewVal);
+   sourceVal->setVal(idxForNewVal, newVal);
+   int rcVal = sourceVal->getVal(idxForNewVal);
+
+   ASSERT_NE(oldVal, rcVal);
+   ASSERT_EQ(newVal, rcVal);
+   delete sourceVal;
+}
+
+TEST_F(CTableTests, getVal_defaultCTORed_3_onIdx_m1_Expect_LimitMin)
+{
+   int idx = -1;
+   CTable* sourceVal = new CTable();
+
+   int rcVal = sourceVal->getVal(idx);
+
+   ASSERT_EQ(std::numeric_limits<int>::min(), rcVal);
+   delete sourceVal;
 }
 }
 
