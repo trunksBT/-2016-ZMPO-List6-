b2951399da3b5815769afe5183c1ae6f82a20599
korytkoBT <209826@student.pwr.wroc.pl>
2016-10-10 00:49:20 +0000

    Remove redundant predefinitions and friend classes

--- TableCtors/TableCtors/Flyweight.h
+++ TableCtors/TableCtors/Flyweight.h
@@ -5,34 +5,8 @@
 #include "CTable.hpp"
 #include "Utils.hpp"
 
-class CreateDefHandler;
-class CreateDefsHandler;
-class CreateCopyHandler;
-class RemoveHandler;
-class RemoveAllHandler;
-class SetNameHandler;
-class GetHandler;
-class CreateCopyHandler;
-class GetNameHandler;
-class GetSizeHandler;
-class GetValueHandler;
-class SetValueHandler;
-class PrintHandler;
-
 class Flyweight
 {
-    friend class CreateDefHandler;
-    friend class CreateDefsHandler;
-    friend class CreateCopyHandler;
-    friend class RemoveHandler;
-    friend class RemoveAllHandler;
-    friend class SetNameHandler;
-    friend class SetValueHandler;
-    friend class GetHandler;
-    friend class GetNameHandler;
-    friend class GetSizeHandler;
-    friend class GetValueHandler;
-    friend class PrintHandler;
 public:
     Flyweight();
     Flyweight(std::vector<std::string>& inCommand,
